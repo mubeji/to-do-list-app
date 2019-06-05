014fbb46ae937e85d98fd1b18110f3a5f4f507e0
wisdom chikwanda <wisdomchikwanda@wisdoms-air.lan>
2019-06-05 16:41:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+/* for unique user id's */
+import uuid from 'uuid'
+//bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import TodoInput from './components/TodoInput'
+import TodoList from './components/TodoList'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <TodoInput />
+                        <TodoList />
+                    </div>
+                </div>
+            </div>
+            
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/TodoInput.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react'
+
+class TodoInput extends Component {
+    render() {
+        return (
+            <div>
+                <h3>TodoInput</h3>
+            </div>
+        )
+    }
+}
+
+export default TodoInput
