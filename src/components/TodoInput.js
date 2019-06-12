9830c8e706b4ae1ef8e09895e979cb64fe7e8be1
wisdom chikwanda <wisdomchikwanda@wisdoms-air.lan>
2019-06-12 10:48:37 +0100

    input component bootstrap

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -3,9 +3,28 @@ import React, { Component } from 'react'
 
 class TodoInput extends Component {
     render() {
+        console.log(this.props)
+        const { item, handleChange, handleSubmit, editItem } = this.props
+
         return (
-            <div>
-                <h3>TodoInput</h3>
+            <div className="card card-body my-3">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <i className="fas fa-book" />
+                            </div>
+                        </div>
+                        <input type="text" className="form-control text-capitalise" 
+                            placeholder="add todo item" value={item} onChange={handleChange}
+                        />
+                    </div>
+                    <button type="submit" 
+                        className="btn btn-block btn-primary mt-3 text-uppercase"
+                    >
+                        add item
+                    </button>
+                </form>
             </div>
         )
     }
